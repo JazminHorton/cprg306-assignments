3f26d243fd513e0859b558d7788eed3c14d68737
JazminHorton <jazmin.horton@edu.sait.ca>
2023-09-15 10:24:42 -0600

    Added files

---
+++ app/StudentInfo.js
@@ -0,0 +1,10 @@
+export function StudentInfo() {
+    return (
+        <div>
+        <h2>Jazmin Horton</h2>
+        <h3>CPRG306-C</h3>
+        <Link href="https://github.com/JazminHorton/cprg306-assignments">Github
+        </Link>
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import { StudentInfo } from "./StudentInfo"
+
+export function MyShoppingList() {
+    return (
+        <div>
+        <h1>My Shopping List</h1>
+        <StudentInfo></StudentInfo>
+        </div>
+    )
+}

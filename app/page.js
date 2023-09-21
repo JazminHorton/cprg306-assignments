451e899dcb973a017b0b0e9c82136fcf0a43cb4e
JazminHorton <jazmin.horton@edu.sait.ca>
2023-09-21 11:45:14 -0600

    Finished Week 2

--- app/page.js
+++ app/page.js
@@ -4,9 +4,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <div>
+      <h1 className="text-5xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+      </div>
+      <div className="flex flex-col text-left">
       <StudentInfo></StudentInfo>
+      </div>
+      <div classname ="flex flex-col text-left">
       <Link href="./week2">week2</Link>
+      </div>
     </main>
   )
 }
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,16 @@
-import { StudentInfo } from "./StudentInfo";
+import { StudentInfo } from "../StudentInfo";
 
-export function MyShoppingList() {
+export default function Home() {
     return (
+  
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
-        <h1>My Shopping List</h1>
-            <StudentInfo></StudentInfo>
+          <h1 className="text-6xl font-bold">My Shopping List</h1>
         </div>
-    )
-}
+        <div className="flex flex-col text-left">
+          <StudentInfo></StudentInfo>
+        </div>
+      </main>
+  
+    );
+  }

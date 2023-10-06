6bc6daa9ca039dcf3b6c0d86d7e0634bce0ee27e
JazminHorton <jazmin.horton@edu.sait.ca>
2023-10-06 09:23:12 -0600

    Added week 5 folder

--- app/week3/item.js
+++ app/week3/item.js
@@ -3,8 +3,8 @@ export default function Item({ name, quantity, category }) {
       <li className="border border-black p-4 mb-4">
         <strong className="font-bold">{name}</strong>
         <p>Quantity: {quantity}</p>
-        <p>Category: {category}</p>
+        <p>Category: {category}</p>   
       </li>
     );
   }
-  
+
---
+++ app/week4/new-item.js
@@ -0,0 +1,118 @@
+"use client";
+import React, { useState } from 'react';
+
+const NewItem = () => {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState('produce');
+  const [error, setError] = useState('');
+  const validateForm = () => {
+    !name || name.trim() === ''
+      ? setError('Name is required')
+      : quantity < 1 || quantity > 99
+      ? setError('Quantity must be between 1 and 99')
+      : setError('');
+    return !error;
+  };
+  
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+  
+  const handleDecrement = () => {
+    quantity > 1 && setQuantity(quantity - 1);
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      const item = {
+        name,
+        quantity,
+        category,
+      };
+      console.log(item);
+      alert(`Name: ${name}\nQuantity: ${quantity}\nCategory: ${category}`);
+      setName('');
+      setQuantity(1);
+      setCategory('produce');
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-black">useStates and Hooks</h2>
+      <form onSubmit={handleSubmit} className="p-2 m-4 bg-black text-black max-w-sm w-full">
+        <div className="mb-2">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Item name"
+            className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+          />
+        </div>
+        <div className="flex justify-between">
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value={quantity}
+            min="1"
+            max="99"
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            required
+            className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+          />
+          <div className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans">
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="" disabled>Category</option>
+              <option value="produce">Produce</option>
+              <option value="dairy">Dairy</option>
+              <option value="bakery">Bakery</option>
+              <option value="meat">Meat</option>
+              <option value="frozen foods">Frozen Foods</option>
+              <option value="canned goods">Canned Goods</option>
+              <option value="dry goods">Dry Goods</option>
+              <option value="beverages">Beverages</option>
+              <option value="snacks">Snacks</option>
+              <option value="household">Household</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+        </div>
+        {error && <p className="error-message">{error}</p>}
+        <button
+          type="button"
+          onClick={handleIncrement}
+          className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={handleDecrement}
+          className="w-full mt-4 py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          -
+        </button>
+        <button
+          type="submit"
+          className="w-full mt-4 py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewItem;

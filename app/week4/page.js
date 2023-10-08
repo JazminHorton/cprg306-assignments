2155cde8f207a6abc8a20129de7c5710cad3cce2
JazminHorton <jazmin.horton@edu.sait.ca>
2023-10-08 15:37:09 -0600

    Finished week4 + Styling

---
+++ app/week4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="flex items-center flex-col">
+            <h1 className="text-2xl">Week 4</h1>
+            <NewItem/>
+        </main>
+    )
+}

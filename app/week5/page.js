d757585cca75611f60ebf4a882e457c8adfe4a4c
JazminHorton <jazmin.horton@edu.sait.ca>
2023-10-14 00:31:20 -0600

    experimenting with styling

--- app/week5/page.js
+++ app/week5/page.js
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 export default function Page() {
     return (
-        <main className='bg-slate-300'>
-            <h1 className='text-2xl ml-4 text-black'>Shopping List</h1>
-            <Link href="/" className='text-2xl ml-4 text-black'>Home</Link>
+        <main>
+            <h1 className='text-2xl ml-4 text-white'>Shopping List</h1>
+            <Link href="/" className='text-2xl ml-4 text-white'>Home</Link>
             <ItemList/>
         </main>
     );

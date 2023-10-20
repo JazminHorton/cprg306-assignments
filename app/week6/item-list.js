dd8570d70bff5a95b6912b8b8a60c67ce6c254dc
JazminHorton <jazmin.horton@edu.sait.ca>
2023-10-20 10:10:45 -0600

    Update item-list.js

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from "react";
 import Item from "./item"; 
-import items from "./items.json"; 
+//import items from "./items.json"; 
 
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");

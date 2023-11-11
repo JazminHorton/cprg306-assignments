14a5c429e8acc6f55231ad168ccb2a5b78cb4d8c
JazminHorton <jazmin.horton@edu.sait.ca>
2023-11-11 15:03:14 -0700

    Styled Home page finally

--- app/page.js
+++ app/page.js
@@ -3,27 +3,58 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-5xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
-      </div>
-      <StudentInfo />
-      <div className="flex items-center text-2xl font-bold">
-        <p>
-          <Link href="/week2">Week 2</Link>
-          <br />
-          <Link href="/week3">Week 3</Link>
-          <br />
-          <Link href="/week4">Week 4</Link>
-          <br />
-          <Link href="/week5">Week 5</Link>
-          <br />
-          <Link href="/week6">Week 6</Link>
-          <br />
-          <Link href="/week7">Week 7</Link>
-          <br />
-          <Link href="/week8">Week 8</Link>
-        </p>
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <div className="bg-black text-white border border-white p-4 rounded-lg text-center">
+        <h1 className="text-6xl text-white mb-6">CPRG-306: Web Development 2 - Assignments</h1>
+        <div className="group hover:text-green-500">
+          <StudentInfo />
+        </div>
+        <ul className="list-none p-0">
+          <div className="list-horizontal mt-4">
+            <ul className="list-none p-0 flex">
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week2">Week 2</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week3">Week 3</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week4">Week 4</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week5">Week 5</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week6">Week 6</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week7">Week 7</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week8">Week 8</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/week10">Week 10</Link>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </ul>
       </div>
     </main>
   );
